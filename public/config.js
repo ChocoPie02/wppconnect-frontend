bba8f88e9e8e54c699a16cc880e4719440b8c8d7
M Zainur <65725993+ChocoPie02@users.noreply.github.com>
2022-12-15 10:09:56 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-window.IP_SERVER = "http://206.189.154.69:21465/api/";
-window.IP_SOCKET_IO = "http://206.189.154.69:21465/";
+window.IP_SERVER = "http://localhost:21465/api/";
+window.IP_SOCKET_IO = "http://localhost:21465/";
 window.IP_SECRET_KEY = "FORTESTING";
